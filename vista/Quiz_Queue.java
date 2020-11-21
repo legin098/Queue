69045f94bdbd5dff3bbe3671046e428c95717a13
legin098 <joven098@hotmail.com>
2020-11-20 20:20:45 -0500

    Uso de metodos Queue aplicaco a un fila de banco

---
+++ vista/Quiz_Queue.java
@@ -0,0 +1,11 @@
+
+package vista;
+
+public class Quiz_Queue {
+
+    public static void main(String[] args) {
+        frame pantalla = new frame();
+        pantalla.setVisible(true);
+    }
+    
+}
